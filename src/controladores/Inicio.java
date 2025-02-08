4dc70112f6f107e8fd8979026ef119a1a2e068d1
MagicalJoker <jluismorego@gmail.com>
2025-02-08 21:06:24 +0100

    Subida de proyecto

---
+++ src/controladores/Inicio.java
@@ -0,0 +1,47 @@
+package controladores;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+		 // Crear una lista de nombres de estudiantes
+        List<String> estudiantes = new ArrayList<>();
+
+        // Agregar nombres a la lista
+        estudiantes.add("Ana");
+        estudiantes.add("Juan");
+        estudiantes.add("Pedro");
+        estudiantes.add("Laura");
+        estudiantes.add("Ana"); 
+
+        // Imprimir la lista completa
+        System.out.println("Lista inicial: " + estudiantes + "\n");
+
+        System.out.println("Cambiar el nombre del tercer estudiante a Carlos");
+        estudiantes.set(2, "Carlos");
+        System.out.println(estudiantes + "\n");
+        
+        
+        System.out.println("Eliminar el nombre \"Laura\" de la lista");
+        estudiantes.remove("Laura");
+        System.out.println(estudiantes + "\n");
+
+        System.out.println("Comprobar si Ana está presente en la lista");
+        if (estudiantes.contains("Ana")) {
+            System.out.println("El nombre 'Ana' está presente en la lista." + "\n");
+        } else {
+            System.out.println("El nombre 'Ana' no está en la lista." + "\n");
+        }
+
+        // Ordenar la lista alfabéticamente
+        Collections.sort(estudiantes);
+
+        System.out.print("Lista ordenada alfabeticamente: ");
+        System.out.println(estudiantes);
+
+	}
+
+}
